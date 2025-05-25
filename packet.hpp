3c97eae71a1decda93c53e689b7e8d2968270cc2
01060c <xxs239239@gmail.com>
2025-05-25 19:06:43 +0530

    read class

--- packet.hpp
+++ packet.hpp
@@ -28,4 +28,24 @@ private:
     std::vector<uint8_t> buffer;
 };
 
+class packetr {
+public:
+    packetr(std::string &s) {}
+
+    int8_t i8();
+    int16_t i16();
+    int32_t i32();
+    int64_t i64();
+    uint8_t u8();
+    uint16_t u16();
+    uint32_t u32();
+    uint64_t u64();
+    float f32();
+    double f64();
+    std::u16string string();
+private:
+    int offset;
+    std::string buffer;
+};
+
 #endif
