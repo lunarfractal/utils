4e1324b3ac9026d9654758c576fbc343ab94f83d
01060c <xxs239239@gmail.com>
2025-05-25 18:59:42 +0530

    chaining.

--- packet.cpp
+++ packet.cpp
@@ -2,61 +2,72 @@
 
 packetw::packetw(int size) : offset(0), buffer(size) {}
 
-void packetw::i8(int8_t v) {
+packetw& packetw::i8(int8_t v) {
     std::memcpy(&buffer[offset], &v, sizeof(int8_t));
     offset += sizeof(int8_t);
+    return *this;
 }
 
-void packetw::i16(int16_t v) {
+packetw& packetw::i16(int16_t v) {
     std::memcpy(&buffer[offset], &v, sizeof(int16_t));
     offset += sizeof(int16_t);
+    return *this;
 }
 
-void packetw::i32(int32_t v) {
+packetw& packetw::i32(int32_t v) {
     std::memcpy(&buffer[offset], &v, sizeof(int32_t));
     offset += sizeof(int32_t);
+    return *this;
 }
 
-void packetw::i64(int64_t v) {
+packetw& packetw::i64(int64_t v) {
     std::memcpy(&buffer[offset], &v, sizeof(int64_t));
     offset += sizeof(int64_t);
+    return *this;
 }
 
-void packetw::u8(uint8_t v) {
+packetw& packetw::u8(uint8_t v) {
     std::memcpy(&buffer[offset], &v, sizeof(uint8_t));
     offset += sizeof(uint8_t);
+    return *this;
 }
 
-void packetw::u16(uint16_t v) {
+packetw& packetw::u16(uint16_t v) {
     std::memcpy(&buffer[offset], &v, sizeof(uint16_t));
     offset += sizeof(uint16_t);
+    return *this;
 }
 
-void packetw::u32(uint32_t v) {
+packetw& packetw::u32(uint32_t v) {
     std::memcpy(&buffer[offset], &v, sizeof(uint32_t));
     offset += sizeof(uint32_t);
+    return *this;
 }
 
-void packetw::u64(uint64_t v) {
+packetw& packetw::u64(uint64_t v) {
     std::memcpy(&buffer[offset], &v, sizeof(uint64_t));
     offset += sizeof(uint64_t);
+    return *this;
 }
 
-void packetw::f32(float v) {
+packetw& packetw::f32(float v) {
     std::memcpy(&buffer[offset], &v, sizeof(float));
     offset += sizeof(float);
+    return *this;
 }
 
-void packetw::f64(double v) {
+packetw& packetw::f64(double v) {
     std::memcpy(&buffer[offset], &v, sizeof(double));
     offset += sizeof(double);
+    return *this;
 }
 
-void packetw::string(std::u16string &v) {
+packetw& packetw::string(std::u16string &v) {
     std::memcpy(&buffer[offset], v.data(), 2 * v.length());
     offset += 2 * v.length();
     buffer.insert(buffer.end(), {0,0});
     offset += 2;
+    return *this;
 }
 
 uint8_t* packetw::data() {
