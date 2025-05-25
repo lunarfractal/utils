b0e727e0c906a01baca715842439750beb33867f
01060c <xxs239239@gmail.com>
2025-05-25 18:51:06 +0530

    Update packet.cpp

--- packet.cpp
+++ packet.cpp
@@ -2,12 +2,63 @@
 
 packetw::packetw(int size) : offset(0), buffer(size) {}
 
-template <typename T>
-packetw::w(T v) {
-    std::memcpy(&buffer[offset], &v, sizeof(T));
-    offset += sizeof(T);
+void packetw::i8(int8_t v) {
+    std::memcpy(&buffer[offset], &v, sizeof(int8_t));
+    offset += sizeof(int8_t);
 }
 
-packetw::data() {
+void packetw::i16(int16_t v) {
+    std::memcpy(&buffer[offset], &v, sizeof(int16_t));
+    offset += sizeof(int16_t);
+}
+
+void packetw::i32(int32_t v) {
+    std::memcpy(&buffer[offset], &v, sizeof(int32_t));
+    offset += sizeof(int32_t);
+}
+
+void packetw::i64(int64_t v) {
+    std::memcpy(&buffer[offset], &v, sizeof(int64_t));
+    offset += sizeof(int64_t);
+}
+
+void packetw::u8(uint8_t v) {
+    std::memcpy(&buffer[offset], &v, sizeof(uint8_t));
+    offset += sizeof(uint8_t);
+}
+
+void packetw::u16(uint16_t v) {
+    std::memcpy(&buffer[offset], &v, sizeof(uint16_t));
+    offset += sizeof(uint16_t);
+}
+
+void packetw::u32(uint32_t v) {
+    std::memcpy(&buffer[offset], &v, sizeof(uint32_t));
+    offset += sizeof(uint32_t);
+}
+
+void packetw::u64(uint64_t v) {
+    std::memcpy(&buffer[offset], &v, sizeof(uint64_t));
+    offset += sizeof(uint64_t);
+}
+
+void packetw::f32(float v) {
+    std::memcpy(&buffer[offset], &v, sizeof(float));
+    offset += sizeof(float);
+}
+
+void packetw::f64(double v) {
+    std::memcpy(&buffer[offset], &v, sizeof(double));
+    offset += sizeof(double);
+}
+
+void packetw::string(std::u16string v) {
+    std::memcpy(&buffer[offset], v.data(), 2 * v.length());
+    offset += 2 * v.length();
+    buffer.insert(buffer.end(), {0,0});
+    offset += 2;
+}
+
+uint8_t* packetw::data() {
     return buffer.data();
 }
