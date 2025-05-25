c6d2e0ea02125be5cbb3dc371e94d0245662950d
01060c <xxs239239@gmail.com>
2025-05-25 18:58:08 +0530

    chaining

--- packet.hpp
+++ packet.hpp
@@ -10,17 +10,17 @@ class packetw {
 public:
     packetw(int size);
 
-    void i8(int8_t v);
-    void i16(int16_t v);
-    void i32(int32_t v);
-    void i64(int64_t v);
-    void u8(uint8_t v);
-    void u16(uint16_t v);
-    void u32(uint32_t v);
-    void u64(uint64_t v);
-    void f32(float v);
-    void f64(double v);
-    void string(std::u16string &v);
+    packetw& i8(int8_t v);
+    packetw& i16(int16_t v);
+    packetw& i32(int32_t v);
+    packetw& i64(int64_t v);
+    packetw& u8(uint8_t v);
+    packetw& u16(uint16_t v);
+    packetw& u32(uint32_t v);
+    packetw& u64(uint64_t v);
+    packetw& f32(float v);
+    packetw& f64(double v);
+    packetw& string(std::u16string &v);
 
     uint8_t* data();
 private:
